f38003c9d6b86e308e191e75b6ecd72c53420497
fastelectronicvegetable <48640397+ckoshka@users.noreply.github.com>
2022-09-26 17:39:53 +1300

    b

---
+++ mod.ts
@@ -0,0 +1,19 @@
+import * as R from "https://esm.sh/ramda@0.28.0";
+
+const trycatch = (fn: R.AnyFunction) => {
+	try {
+		return fn();
+	} catch (e) {
+		return new Error(e);
+	}
+};
+
+export const aPipe = R.pipeWith((fn, r) => Promise.resolve(r).then(fn));
+
+export const tryPipe = R.pipeWith((fn, r) =>
+	r instanceof Error ? r : trycatch(() => fn(r))
+);
+
+export const nullPipe = R.pipeWith((fn, r) =>
+	r === null || r === undefined ? r : fn(r)
+);
